ea1b419ef3e36ce09fb238e4f91d39a464d56ec8
GamerNoTitle <admin@bili33.top>
2022-01-23 23:21:46 +0800

    Optimized logic of the plugin

--- MirrorServerReforged.py
+++ MirrorServerReforged.py
@@ -1,4 +1,5 @@
 from email.headerregistry import Address
+from http import server
 from importlib.resources import path
 import shutil
 import os
@@ -51,16 +52,28 @@ def InitalizeOnFirstRun():
         global MCDR
         MCDR = True     # Turn on MCDR mode
     if not os.path.exists('./Mirror'):
+        print('[MirrorServerReforged] 看起来你是第一次运行本插件？我们将会为您进行首次运行的初始化')
+        print('[MirrorServerReforged] 正在创建镜像文件夹……')
         if MCDR:    # MCDR mode on, create Mirror folder and a server folder in Mirror folder
-            os.makedirs('./Mirror')
+            print('[MirrorServerReforged] 检测到MCDR，我们将会按照MCDR的目录结构创建文件夹')
+            try:
+                os.makedirs('./Mirror')
+            except:
+                print('[MirrorServerReforged] Mirror文件夹已存在！')
             os.makedirs('./Mirror/server')
         else:   # MCDR mode off, turn into legacy mode. Like Vanilla, Bukkit, Waterfalls and so on.
-            os.makedirs('./Mirror')
+            print('[MirrorServerReforged] 未检测到MCDR，我们将会按照普通服务器的目录结构创建文件夹')
+            try:
+                os.makedirs('./Mirror')
+            except:
+                print('[MirrorServerReforged] Mirror文件夹已存在！')
             for world in config['world']:
                 os.makedirs('./Mirror/{}'.format(world))
+        print('[MirrorServerReforged] 初始化完成！')
 
 
 def CreateConfig():
+    print('[MirrorServerReforged] 正在创建配置文件……')
     global config
     with open('./config/MirrorServerReforged.json', 'w', encoding="utf-8") as f:
         f.write(json.dumps(config, indent=2, separators=(
@@ -72,18 +85,23 @@ def RconInit(host, port, password):
     Rcon = RconConnection(host, port, password)
     return Rcon
 
+
 def LoadConfig():
+    print('[MirrorServerReforged] 正在加载配置文件……')
     global config
     with open('./config/MirrorServerReforged.json', 'r', encoding="utf-8") as f:
         config = json.load(f)
 
 
-def Sync():
+def Sync(server):
+    server.reply('§b[MirrorServerReforged] §6正在同步服务器地图……')
     if os.path.exists('./Mirror/world') and MCDR == True:
         shutil.copytree('./server/world/', './Mirror/world/')
     else:
         for world in config['world']:
-            shutil.copytree('./server/{}/'.format(world), './Mirror/{}/'.format(world))
+            shutil.copytree('./server/{}/'.format(world),
+                            './Mirror/{}/'.format(world))
+    server.reply('§b[MirrorServerReforged] §6同步完成！')
 
 
 @new_thread('MirrorServerReforged')
@@ -92,8 +110,12 @@ def Start(server):
     if Started:
         server.reply('b[MirrorServerReforged] §6镜像服正在运行……')
     else:
+        server.reply('§b[MirrorServerReforged] §6正在启动镜像服，这可能需要一定的时间……')
+        server.reply(
+            '§b[MirrorServerReforged] §6启动完成后，请自行利用BungeeCord的转服或者直连进行转服！')
         Started = True
         os.system(config['command'])
+        server.reply('§b[MirrorServerReforged] §6镜像服已关闭！')
     Started = False
 
 
@@ -103,9 +125,11 @@ def Status(server):
     else:
         server.reply('§b[MirrorServerReforged] §6镜像服未运行……')
 
+
 def Stop(server):
     if config['rcon']['enable']:
-        conn = RconInit(config['rcon']['host'], config['rcon']['port'], config['rcon']['password'])
+        conn = RconInit(config['rcon']['host'], config['rcon']
+                        ['port'], config['rcon']['password'])
         connected = conn.connect()
         if connected:
             conn.send('stop')
@@ -113,14 +137,16 @@ def Stop(server):
             global Started
             Started = False
         else:
-            server.reply('§b[MirrorServerReforged] §6Rcon连接失败，请检查网络原因或配置信息是否正确！')
+            server.reply(
+                '§b[MirrorServerReforged] §6Rcon连接失败，请检查网络原因或配置信息是否正确！')
     else:
         server.reply('§b[MirrorServerReforged] §6无法在主服务器停止宿服务器，因为Rcon未开启！')
 
 
-
-def Reload():
+def Reload(server):
+    server.reply('§b[MirrorServerReforged] §6正在重载配置文件……')
     ConfigToDo()
+    server.reply('§b[MirrorServerReforged] §6重载完成！')
 
 
 def DisplayHelp(server):
@@ -146,4 +172,3 @@ def on_load(server, prev):
                             .then(Literal('start').runs(Start))
                             .then(Literal('stop').runs(Stop))
                             )
-
