a884d3655a64391c8ade756cbffb833df3fa5dc5
GamerNoTitle <GamerNoTitle@outlook.com>
2023-02-23 22:05:26 +0800

    xcopy in Windows

--- mirror_server_reforged/__init__.py
+++ mirror_server_reforged/__init__.py
@@ -123,7 +123,8 @@ def ServerSync(InterFace):
                 shutil.rmtree(f'./Mirror/server/{world}/')
             # shutil.copytree('./server/world/', './Mirror/world/', ignore=ignore)
             if sys.platform == 'win32':
-                os.system(f'copy ./server/{world} ./Mirror/server/{world}')
+                os.mkdir(f'./Mirror/server/{world}')
+                os.system(f'xcopy "./server/{world}" "./Mirror/server/{world}" /e /h /k /y')
             else:
                 os.system(f'cp -r ./server/{world} ./Mirror/server/{world}')
     else:
@@ -131,7 +132,8 @@ def ServerSync(InterFace):
             if os.path.exists(f'./Mirror/{world}'):
                 shutil.rmtree(f'./Mirror/{world}/')
             if sys.platform == 'win32':
-                os.system(f'copy ./server/{world} ./Mirror/{world}')
+                os.mkdir(f'./Mirror/{world}')
+                os.system(f'xcopy "./server/{world}" "./Mirror/{world}" /e /h /k /y')
             else:
                 os.system(f'cp -r ./server/{world} ./Mirror/{world}')
     end_time = datetime.datetime.now()
